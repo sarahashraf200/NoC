0f7d2fde9aa7be424a70940685027d78e92bbe2a
Mo3az99 <42701773+Mo3az99@users.noreply.github.com>
2020-06-03 03:11:42 +0200

    Add files via upload

--- router.vhd
+++ router.vhd
@@ -1,35 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    02:44:24 05/20/2020 
--- Design Name: 
--- Module Name:    router - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
-
 library ieee;
 use ieee.std_logic_1164.all;
-
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity router is
 	port(
